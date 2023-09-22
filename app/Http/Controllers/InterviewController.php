844eeb25ecddf744effb0ff68d2be61168bb637a
CoreyTaylorNepomuceno <100895783+CoreyTaylorNepomuceno@users.noreply.github.com>
2023-09-22 21:44:08 +0800

    Added screening form <3

--- app/Http/Controllers/InterviewController.php
+++ app/Http/Controllers/InterviewController.php
@@ -9,4 +9,8 @@ class InterviewController extends Controller
     function ShowPendingInterview(){
         return view('admin.dashboard-pending-interview');
     }
+
+    function ShowScreeningForm(){
+        return view('admin.dashboard-screening-form');
+    }
 }
